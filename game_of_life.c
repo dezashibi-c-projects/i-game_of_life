71adbb458ead94fb6b36e5304e2a1ef188b1fd6a
Navid Dezashibi <dezashibi@gmail.com>
2024-07-31 14:16:16 +0400

    initial commit 2

---
+++ game_of_life.c
@@ -0,0 +1,23 @@
+// ***************************************************************************************
+//    Project: Conway's Game of Life in Console using C
+//    File: game_of_life.c
+//    Date: 2024-07-31
+//    Author: Navid Dezashibi
+//    Contact: navid@dezashibi.com
+//    Website: https:www.dezashibi.com | https://github.com/dezashibi
+//    License:
+//     Please refer to the LICENSE file, repository or website for more information about
+//     the licensing of this work. If you have any questions or concerns,
+//     please feel free to contact me at the email address provided above.
+// ***************************************************************************************
+// *  Description: Read the readme file for more information
+// ***************************************************************************************
+
+#include <stdio.h>
+
+int main()
+{
+    puts("Well hello there!");
+
+    return 0;
+}
